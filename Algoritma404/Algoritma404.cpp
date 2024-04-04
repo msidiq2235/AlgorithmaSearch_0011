98947c056420614c0f2708a0a5d17ce16f599eaa
msidiq2235 <muhsidiq2005@gmail.com>
2024-04-04 17:00:18 +0700

    3

--- Algoritma404/Algoritma404.cpp
+++ Algoritma404/Algoritma404.cpp
@@ -5,3 +5,13 @@ int arr[20]; // Array to be searched
 int n; // Number of elements in the array
 int i; // Index of array element
 
+void input()
+{
+	while (true)
+	{
+		cout << "Enter the number of elements in the array; ";
+		cin >> n;
+		if ((n > 0) && (n <= 20))
+			break;
+	}
+}
